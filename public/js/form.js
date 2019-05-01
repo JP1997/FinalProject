e86272f40a7fbfd112d89edd85b1750f3eb6cce6
Arturo Ramirez <arturo.rama97@gmail.com>
2019-05-01 16:31:34 -0500

    modified form.js

--- public/js/form.js
+++ public/js/form.js
@@ -1,43 +1,127 @@
-let searchBox =$('.searchBox').val();
 
 
-let doctorName =$('.doctorName').val();
-let nombrePaciente =$('.nombrePaciente').val();
-let edadPaciente = $('.edadPaciente').val();
-let ocupacionPaciente =$('.ocupacionPaciente').val();
-let direccionPaciente =$('.dirrecionPaciente').val();
-let telefonoPaciente =$('.telefonoPaciente').val();
-let ciudadPaciente =$('.ciudadPaciente').val();
-let estadoPaciente =$('.estadoPaciente').val();
-let motivoPaciente =$('.motivoPaciente').val();
-let historialClinico =$('.historialClinico').val();
-let observacionesDerecho =$('.observacionesDerecho').val();
-let observacionesIzquierdo =$('.observacionesIzquierdo').val();
-let RXAnteriorODESF =$('RXAnteriorODESF').val();
-let RXAnteriorOIESF =$('.RXAnteriorOIESF').val();
-let AgudezaODSL =$('.AgudezaODSL').val();
-let AgudezaODAE =$('.AgudezaODAE').val();
-let AgudezaODCV =$('.AgudezaODCV').val();
-let AgudezaOISL =$('.AgudezaOISL').val();
-let AgudezaOIAE =$('.AgudezaOIAE').val();
-let AgudezaOICV =$('.AgudezaOICV').val();
-let RetOD =$('.RetOD').val();
-let RetOD20 =$('.RetOD20').val();
-let RetOI =$('.RetOI').val();
-let RetOI20 =$('.RetOI20').val();
-let SubjetivoOD =$('.SubjetivoOD').val();
-let SubjetivoODCVL =$('SubjetivoODCVL').val();
-let SubjetivoODCVC =$('SubjetivoODCVC').val();
-let SubjetivoOI =$('.SubjetivoOI').val();
-let SubjetivoOICVL =$('SubjetivoOICVL').val();
-let SubjetivoOICVC =$('SubjetivoOICVC').val();
-let RxFinalODESF =$('RxFinalODESF').val();
-let RxFinalODADD =$('RxFinalODADD').val();
-let RxFinalODAV =$('RxFinalODAV').val();
-let tipoLente =$('tipoLente').val();
-let material =$('material').val();
-let color =$('color').val();
-let RxFinalODESF =$('RxFinalOIESF').val();
-let RxFinalODADD =$('RxFinalOIADD').val();
-let RxFinalODAV =$('RxFinalOIDAV').val();
-let observacionesFinales =$('observacionesFinales').val();
+function searchPatient(){
+
+}
+
+function watchForm(){
+	$('.historialForm').on('submit', function(event) {
+		event.preventDefault();
+
+		let doctorName =$('.doctorName').val();
+		let nombrePaciente =$('.nombrePaciente').val();
+		let edadPaciente = $('.edadPaciente').val();
+		let ocupacionPaciente =$('.ocupacionPaciente').val();
+		let direccionPaciente =$('.direccionPaciente').val();
+		let telefonoPaciente =$('.telefonoPaciente').val();
+		let ciudadPaciente =$('.ciudadPaciente').val();
+		let estadoPaciente =$('.estadoPaciente').val();
+		let motivoPaciente =$('.motivoPaciente').val();
+		let historialClinico =$('.historialClinico').val();
+		let observacionesDerecho =$('.observacionesDerecho').val();
+		let observacionesIzquierdo =$('.observacionesIzquierdo').val();
+		let RXAnteriorODESF =$('.RXAnteriorODESF').val();
+		let RXAnteriorOIESF =$('.RXAnteriorOIESF').val();
+		let AgudezaODSL =$('.AgudezaODSL').val();
+		let AgudezaODAE =$('.AgudezaODAE').val();
+		let AgudezaODCV =$('.AgudezaODCV').val();
+		let AgudezaOISL =$('.AgudezaOISL').val();
+		let AgudezaOIAE =$('.AgudezaOIAE').val();
+		let AgudezaOICV =$('.AgudezaOICV').val();
+
+		let RetOD =$('.RetOD').val();
+		let RetOD20 =$('.RetOD20').val();
+		let RetOI =$('.RetOI').val();
+		let RetOI20 =$('.RetOI20').val();
+
+		let SubjetivoOD =$('.SubjetivoOD').val();
+		let SubjetivoODCVL =$('.SubjetivoODCVL').val();
+		let SubjetivoODCVC =$('.SubjetivoODCVC').val();
+		let SubjetivoOI =$('.SubjetivoOI').val();
+		let SubjetivoOICVL =$('.SubjetivoOICVL').val();
+		let SubjetivoOICVC =$('.SubjetivoOICVC').val();
+
+		let RxFinalODESF =$('.RxFinalODESF').val();
+		let RxFinalODADD =$('.RxFinalODADD').val();
+		let RxFinalODAV =$('.RxFinalODAV').val();
+
+		let tipoLente =$('.tipoLente').val();
+		let material =$('.material').val();
+		let color =$('.color').val();
+		let RxFinalOIESF =$('.RxFinalOIESF').val();
+		let RxFinalOIADD =$('.RxFinalOIADD').val();
+		let RxFinalOIAV =$('.RxFinalOIAV').val();
+		let observacionesFinales =$('.observacionesFinales').val();
+
+		let data= {
+			nombre: nombrePaciente,
+			doctor : doctorName,
+			edad : edadPaciente,
+			ocupacion: ocupacionPaciente,
+			direccion : direccionPaciente,
+			telefono : telefonoPaciente,
+			ciudad : ciudadPaciente,
+			estado : estadoPaciente,
+			motivo : motivoPaciente,
+			historialClinico : historialClinico,
+			ojoDerecho : {
+							observaciones : observacionesDerecho,
+							RXAnteriorESF : RXAnteriorODESF,
+							agudezaVisual : {
+												SL : AgudezaODSL,
+												AE : AgudezaODAE,
+												CV : AgudezaODCV
+							},
+							retinoscopia : RetOD,
+							retinoscopia20 : RetOD20,
+							exSubjetivo : SubjetivoOD,
+							exSubjetivoCVL : SubjetivoODCVL,
+							exSubjetivoCVC : SubjetivoODCVC,
+							RXFinalESF : RxFinalODESF,
+							RXFinalADD : RxFinalODADD,
+							RXFinalAV : RxFinalODAV,
+			},
+			ojoIzquierdo :{
+							observaciones : observacionesIzquierdo,
+							RXAnteriorESF : RXAnteriorOIESF,
+							agudezaVisual : {
+												SL : AgudezaOISL,
+												AE : AgudezaOIAE,
+												CV : AgudezaOICV
+							},
+							retinoscopia : RetOI,
+							retinoscopia20 : RetOI20,
+							exSubjetivo : SubjetivoOI,
+							exSubjetivoCVL : SubjetivoOICVL,
+							exSubjetivoCVC : SubjetivoOICVC,
+							RXFinalESF : RxFinalOIESF,
+							RXFinalADD : RxFinalOIADD,
+							RXFinalAV : RxFinalOIAV
+			},
+			tipoLente : tipoLente,
+			material : material,
+			color : color,
+			observacionFinal: observacionesFinales
+		};
+		console.log(data);
+	});
+
+	$('.submitPatient').on('click', function(event) {
+		let searchBox =$('.searchBox').val();
+		
+	});
+}
+
+function onload(){
+	$('.searchPatient').show();
+	$('.historialForm').hide();
+}
+
+function init(){
+	$(watchForm);
+	$(onload);
+
+}
+
+
+$(init);
